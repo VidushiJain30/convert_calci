71c8e05bd67a1ecc95b10fc318176ea8ab426b7b
VidushiJain30 <jainvidushi30@gmail.com>
2018-07-07 00:49:55 +0530

    dropdown applied

--- calci.js
+++ calci.js
@@ -1,6 +1,18 @@
-function select_unit ()
+function select_unit (y)
 	{
-		var x=document.getElementById("men");
-		x.style.display="block";
-		
+		var x=document.getElementById("len_sel");
+		x.style.display="none";
+		var x=document.getElementById("ar_sel");
+		x.style.display="none";
+		var x=document.getElementById("vol_sel");
+		x.style.display="none";
+		if (y=='len')
+		{var x=document.getElementById("len_sel");
+		x.style.display="block";}
+		else if(y=='area')
+		{var x=document.getElementById("ar_sel");
+		x.style.display="block";}
+		else if(y=='vol')
+			{var x=document.getElementById("vol_sel");
+		x.style.display="block";}	
 	}
